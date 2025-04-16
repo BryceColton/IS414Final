97e3e00223e915fd91986fba71c6d9a5e706cb0e
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-04-16 15:52:13 -0600

    pushes .gitignore should be fixix

---
+++ backendAPI/backendAPI/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using backendAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var dbPath = Path.Combine(AppContext.BaseDirectory, "Bookstore.sqlite");
+builder.Services.AddDbContext<BooksDbContext>(options =>
+    options.UseSqlite($"Data Source={dbPath}"));
+
+builder.Services.AddCors(options =>
+    options.AddPolicy("AllowReactApp",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    }));
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowReactApp");
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
